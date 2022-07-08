52174f9981f2cf11525ccab1ef30ab2e0ac2edfd
fatihtuzunn <fatihtuzun11@gmail.com>
2022-07-09 02:20:49 +0300

    add exception handling

--- project_1.py
+++ project_1.py
@@ -3,33 +3,37 @@ import pandas as pd
 
 class student():
     
-  
-        
-    
     def __init__(self, name, surname, number, score):
         self.name = name
         self.surname = surname
         self.number = number
         self.score = score
+        self.findStatus()
+        
+    def findStatus(self):
         global letterscore
-        if 100 >= self.score >= 90:
-            self.status = True
-            letterscore = "A"
-        elif 90 > self.score >= 80:
-            self.status = True
-            letterscore = "B"
-        elif 80 > self.score >= 70:
-            self.status = True
-            letterscore = "C"
-        elif 70 > self.score >= 60:
-            self.status = True
-            letterscore = "D"
-        elif 60 > self.score >= 50:
-            self.status = True
-            letterscore = "E"
-        elif 50 > self.score >= 0:
-            self.status = False
-            letterscore = "F"
+        if 100 >= self.score >= 0:
+            if 100 >= self.score >= 90:
+                self.status = True
+                letterscore = "A"
+            elif 90 > self.score >= 80:
+                self.status = True
+                letterscore = "B"
+            elif 80 > self.score >= 70:
+                self.status = True
+                letterscore = "C"
+            elif 70 > self.score >= 60:
+                self.status = True
+                letterscore = "D"
+            elif 60 > self.score >= 50:
+                self.status = True
+                letterscore = "E"
+            elif 50 > self.score >= 0:
+                self.status = False
+                letterscore = "F"
+        else:
+            print("Puanınızı doğru girdiğinizden emin olun!")
+    
 
     def learnStatus(self):
         if self.status:
@@ -42,18 +46,34 @@ class student():
 studentList = []
 
 # girilecek element sayisi
-n = int(input("Kaç öğrenci gireceksiniz?: "))
+try:
+    n = int(input("Kaç öğrenci gireceksiniz?: "))
 
-# girilecek elemente göre objecti listeye ekliyoruz
-for i in range(0, n):
-    print(f"\n Girdi {i+1} \n")
-    newName = input("İsminiz: ")
-    newSurname = input("Soyisminiz: ")
-    newNumber = int(input("Okul numaranız: "))
-    newScore = int(input("Sınav notunuz: "))
+    # girilecek elemente göre objecti listeye ekliyoruz
+    for i in range(0, n):
+        
+        print(f"\n Girdi {i+1} \n")
+        newName = input("İsminiz: ")
+        newSurname = input("Soyisminiz: ")
+        newNumber = int(input("Okul numaranız: "))
+        newScore = int(input("Sınav notunuz: "))
 
-    # adding the element
-    studentList.append(student(newName, newSurname, newNumber, newScore))
+        # adding the element
+        studentList.append(student(newName, newSurname, newNumber, newScore))
+
+    
+    df = pd.DataFrame(obj.__dict__ for obj in studentList)
+
+    print(df)
+
+except ValueError:
+        print("Değerleri doğru girdiğinizden emin olun!") 
+
+
+
+
+
+
 
 
 """ 
@@ -78,7 +98,3 @@ def dataf():
 df = pd.DataFrame(dataf())
 """
 
-
-df = pd.DataFrame(obj.__dict__ for obj in studentList)
-
-print(df)
