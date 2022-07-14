e62ee24afe024bd019e0881632dbe3b6914bc1c0
Aydın Kabataş <aydin.kabatas@hotmail.com>
2022-07-14 23:53:45 +0300

    Try-Except yapısı ve bir takım düzeltmeler. (A)

--- project_1.py
+++ project_1.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import openpyxl
 
-
-
 class student():
     
     def __init__(self, name, surname, number, score):
@@ -14,46 +12,44 @@ class student():
         
     def findStatus(self):
         
-        if 100 >= self.score >= 0:
-            if 100 >= self.score >= 90:
-                self.status = True
-                self.letterscore = "A"
-            elif 90 > self.score >= 80:
-                self.status = True
-                self.letterscore = "B"
-            elif 80 > self.score >= 70:
-                self.status = True
-                self.letterscore = "C"
-            elif 70 > self.score >= 60:
-                self.status = True
-                self.letterscore = "D"
-            elif 60 > self.score >= 50:
-                self.status = True
-                self.letterscore = "E"
-            elif 50 > self.score >= 0:
-                self.status = False
-                self.letterscore = "F"
+        # Giris kisminda kontrol yapildigi icin kontroller kaldirildi. (A)
+        if 100 >= self.score >= 90:
+            self.status = True
+            self.letterscore = "A"
+        elif 90 > self.score >= 80:
+            self.status = True
+            self.letterscore = "B"
+        elif 80 > self.score >= 70:
+            self.status = True
+            self.letterscore = "C"
+        elif 70 > self.score >= 60:
+            self.status = True
+            self.letterscore = "D"
+        elif 60 > self.score >= 50:
+            self.status = True
+            self.letterscore = "E"
         else:
-            print("Puanınızı doğru girdiğinizden emin olun!")
+            self.status = False
+            self.letterscore = "F"
         
-        if self.status: self.userstatus="Gecti" 
-        else: self.userstatus="Kaldı" 
-        
-    
+        if self.status==True: self.userstatus= "Gecti"
+        else: self.userstatus="Kaldi" # IF kosulu revize edildi. (A)
 
+    # Kullanilmadiği icin kapatildi. Silinebilir. Tartismaya acik. (A)
+    """
     def learnStatus(self):
         if self.status:
-            print(f"Tebrikler {letterscore} notu ile geçtiniz.")
+            print(f"Tebrikler {letterscore} notu ile gectiniz.")
 
         else:
-            print(f"Malesef {letterscore} notu ile kaldınız.")
-
+            print(f"Maalesef {letterscore} notu ile kaldiniz.")
+    """
 
 studentList = []
 
 # girilecek element sayisi
 try:
-    n = int(input("Kaç öğrenci gireceksiniz?: "))
+    n = int(input("Kac ogrenci gireceksiniz?: "))
 
     # girilecek elemente göre objecti listeye ekliyoruz
     for i in range(0, n):
@@ -61,12 +57,19 @@ try:
         print(f"\n Girdi {i+1} \n")
         newName = input("İsminiz: ")
         newSurname = input("Soyisminiz: ")
-        newNumber = int(input("Okul numaranız: "))
-        newScore = int(input("Sınav notunuz: "))
+        newNumber = input("Okul numaraniz: ") 
+        # Bazi okul numaralarinda string karakterler olabiliyor. O sebeple int ibaresini kaldirdim. (A)
+        # Sinav notu icin try-except yapildi. Hatasiz girilmesi adina while dongusu yapildi. (A)
+        while True:    
+            try:
+                newScore = int(input("Sinav notunuz: "))
+                if 0 <= newScore <= 100: break
+                else: print("Sinav notunuz 0 ile 100 arasinda olmalidir. Lütfen tekrar giriniz.")
+            except ValueError():
+                print("Lütfen sinav notunuzu sayisal olarak giriniz.")
 
         # adding the element
         studentList.append(student(newName, newSurname, newNumber, newScore))
-
     
     df = pd.DataFrame(obj.__dict__ for obj in studentList)
 
@@ -75,26 +78,32 @@ try:
     #excel cıktısı
     df.to_excel("output.xlsx")
 
+
+ 
+    #Kalan öğrencilerle geçen öğrencileri ayrı listelere ayırdım.
+    
+    failStudents = []
+    successStudents= []
+    for stu in studentList:
+        if stu.status:
+            successStudents.append(stu)
+        else:
+            failStudents.append(stu)
+
+    suc  = pd.DataFrame(obj.__dict__ for obj in successStudents)
+    fai  = pd.DataFrame(obj.__dict__ for obj in failStudents)
+
+    # Daha toplu gozukmesi adina kucuk rotuslar :) (A)
+    print("--------------------")
+    print("Dersi gecen ogrencilerin listesi:")
+    print(suc)
+
+    print("--------------------")
+    print("Dersten kalan ogrencilerin listesi:")
+    print(fai)
+
 except ValueError:
-        print("Değerleri doğru girdiğinizden emin olun!") 
-
-
-#Kalan öğrencilerle geçen öğrencileri ayrı listelere ayırdım.
-
-failStudents = []
-successStudents= []
-for stu in studentList:
-    if stu.status:
-        successStudents.append(stu)
-    else:
-        failStudents.append(stu)
-
-suc  = pd.DataFrame(obj.__dict__ for obj in successStudents)
-fai  = pd.DataFrame(obj.__dict__ for obj in failStudents)
-
-print(suc,fai)
-
-
+        print("Ogrenci sayiniz hatali, programdan cikiliyor...") 
 
 
 
